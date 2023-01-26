53eec77c126ce0985138517316b111f137114340
gyojincompany <luckyred@gmail.com>
2023-01-26 15:32:54 +0900

    get / post 방식 처리 기능 추가

--- src/main/java/com/gyojincompany/exer/FrontController.java
+++ src/main/java/com/gyojincompany/exer/FrontController.java
@@ -29,6 +29,58 @@ public class FrontController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		doAction(request, response);
+		
+//		String uri = request.getRequestURI();
+//		
+//		System.out.println("가져온 uri : "+uri);
+//		
+//		String conPath = request.getContextPath();//컨택스트 패스만 분리하여 저장
+//		
+//		System.out.println("Context Path : "+conPath);
+//		
+//		String command = uri.substring(conPath.length());
+//		
+//		System.out.println("command : "+command);
+//		
+//		if(command.equals("/BlogHome.naver")) {
+//			System.out.println("BlogHome 명령이 들어왔습니다.");
+//			request.setAttribute("id", "tiger");
+//			//response.sendRedirect("blog.jsp");
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("/blog.jsp");
+//			dispatcher.forward(request, response);
+//			
+//		} else if(command.equals("/Hello.naver")) {
+//			System.out.println("Hello 명령이 들어왔습니다.");
+//			//response.sendRedirect("hello.jsp");
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("/hello.jsp");
+//			dispatcher.forward(request, response);
+//		} else if(command.equals("/Login.naver")) {
+//			
+//			String mid = request.getParameter("id");
+//			String mpw = request.getParameter("pw");
+//			
+//			System.out.println("Login 명령이 들어왔습니다.");
+//			System.out.println("id : " + mid);
+//			System.out.println("pw : " + mpw);
+//			
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("/loginOk.jsp");
+//			dispatcher.forward(request, response);
+//		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		String uri = request.getRequestURI();
 		
 		System.out.println("가져온 uri : "+uri);
@@ -65,15 +117,6 @@ public class FrontController extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/loginOk.jsp");
 			dispatcher.forward(request, response);
 		}
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -7,7 +7,7 @@
 <title>회원 로그인</title>
 </head>
 <body>
-	<form action="Login.naver">
+	<form action="Login.naver" method="post">
 		아이디 : <input type="text" name="id"> <br><br>
 		비밀번호 : <input type="text" name="pw"> <br><br>
 		<input type="submit" value="로그인">
